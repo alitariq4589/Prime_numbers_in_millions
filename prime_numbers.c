6d7cc428b0c9787fe27bd4cc9fc431319ff2d298
Ali Tariq <ali.tariq@10xengineers.ai>
2022-12-05 21:25:14 +0500

    Added jenkinsfile and prime_numbers.c

---
+++ prime_numbers.c
@@ -0,0 +1,16 @@
+{
+    int IsNotPrime;
+    for (int i = 2; i <= 1000000; i++)
+    {
+        IsNotPrime = 0;
+        for (int j = 2; j <= i / 2; j++)
+        {
+            if (i % j == 0)
+            {
+                IsNotPrime++;
+            }
+        }
+        if (IsNotPrime == 0)
+            printf("%d\n", i);
+    }
+}
